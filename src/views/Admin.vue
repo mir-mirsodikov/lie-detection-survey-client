fe59f629cc3207ffcaf7451c0983dddcdd92df29
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2022-05-08 19:42:24 -0500

    Ui element for creating new question

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,8 +1,22 @@
 <template>
   <h1>Admin</h1>
 
-  <div>
-    
+  <div class="uk-card uk-card-default uk-card-hover uk-margin-auto uk-width-1-2">
+    <div class="uk-card-header">
+      <h3 class="uk-card-title">Create new question</h3>
+    </div>
+
+    <div class="uk-card-body">
+      <form class="uk-form-stacked">
+        <fieldset class="uk-fieldset">
+          <div class="uk-margin">
+            <textarea class="uk-textarea" placeholder="Enter new question" />
+          </div>
+
+          <button class="uk-button uk-button-primary uk-align-right uk-margin-remove">Submit</button>
+        </fieldset>
+      </form>
+    </div>
   </div>
 </template>
 

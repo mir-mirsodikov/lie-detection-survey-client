77da1c6362cfee0e30b500c20877805afc982f26
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2022-05-09 15:55:16 -0500

    Start of edit question feature

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -4,11 +4,12 @@
       <h3 class="uk-card-title">Question ID: {{ question.id }}</h3>
     </div>
     <div class="uk-card-body">
-      <p>{{ question.value }}</p>
+      <p v-if="!edit" @click="editQuestion" >{{ question.value }}</p>
+      <textarea class="uk-textarea" v-model="question.value" v-if="edit" @keyup.enter="editQuestion"></textarea>
     </div>
     <div class="uk-card-footer">
       <div class="uk-align-right">
-        <a uk-icon="file-edit" class="uk-icon-link uk-margin-right"></a>
+        <a uk-icon="file-edit" class="uk-icon-link uk-margin-right" @click="editQuestion"></a>
         <a uk-icon="trash" class="uk-icon-link"></a>
       </div>
     </div>
@@ -16,6 +17,8 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 defineProps<{
   question: {
     id: number,
@@ -23,4 +26,10 @@ defineProps<{
     active: boolean,
   }
 }>();
+
+const edit = ref(false);
+
+const editQuestion = () => {
+  edit.value = !edit.value;
+};
 </script>
